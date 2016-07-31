8476ae1e2a4641be6dd8eda1706b0bfffca99f14
Gagan Deep Singh <Gagandeep.singh@barco.com>
2016-07-31 17:00:52 +0530

    start

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Test");
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('ready', function () {
+   mainWindow = new BrowserWindow({
+       width:800,
+       height: 800
+   })
+    mainWindow.loadURL("file://" + __dirname + "/index.html");
+});
